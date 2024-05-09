b0fde44b53e009d9512440e23101d33d6b33ff76
Jonathan Nielsen <jonathan.sparvath@gmail.com>
2024-05-09 15:20:43 +0200

    using real database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+const mockUrls = [
+  "https://utfs.io/f/4f58b5b2-6a35-49c9-a4bf-f69cc8dd1c2e-eilcxh.webp",
+  "https://utfs.io/f/8ca833b8-0889-454d-a3fe-041401697e7a-1n7x8s.webp",
+  "https://utfs.io/f/565dbd57-4753-4838-9c56-5d365fd41365-eilcyc.webp",
+  "https://utfs.io/f/7c69d292-cc6f-4f55-bc2f-b6ee3224df4d-vpfo60.webp",
+];
+
+const mockImages = mockUrls.map((url, i) => ({
+  id: i + 1,
+  url,
+}));
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log({ posts });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        Hello gallery in progress
+    <main className="">
+      <div className="gp flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + `${index}`}>
+            <img className="w-48 p-4" src={image.url} alt="image" />
+          </div>
+        ))}
       </div>
     </main>
   );
