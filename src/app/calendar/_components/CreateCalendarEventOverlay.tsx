f5595e5b41ceb322245d41cc669787b4db7c78ed
Jonathan Nielsen <jonathan.sparvath@gmail.com>
2024-08-19 21:31:30 +0200

    revert create calendar date to before

--- src/app/calendar/_components/CreateCalendarEventOverlay.tsx
+++ src/app/calendar/_components/CreateCalendarEventOverlay.tsx
@@ -36,7 +36,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "~/components/ui/popover";
-import { cn, getTimeFromDateHourTwoDigits } from "~/lib/utils";
+import { cn } from "~/lib/utils";
 import { Calendar } from "~/components/ui/calendar";
 import { Textarea } from "~/components/ui/textarea";
 import {
@@ -84,9 +84,6 @@ export function CreateCalendarEventOverlay() {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     const { date, time, ...rest } = values;
-    // console.log({ time });
-    // console.log({ hour: time?.hour });
-    // console.log({ minute: time?.minute });
     const dateString = new Date(
       Date.UTC(
         date.getFullYear(),
@@ -95,18 +92,9 @@ export function CreateCalendarEventOverlay() {
         time?.hour ?? 23,
         time?.minute ?? 59,
       ),
-    );
+    ).toISOString();
 
-    // const time = getTimeFromDateHourTwoDigits(dateObject);
-    const userTimezoneOffset = dateString.getTimezoneOffset() * 60000;
-    // var userTimezoneOffset = date.getTimezoneOffset() * 60000;
-    const newDate = new Date(dateString.getTime() + -1 * userTimezoneOffset);
-    console.log({ hm: getTimeFromDateHourTwoDigits(newDate) });
-    const newDateString = newDate.toISOString();
-    console.log({ newDateString });
-    // console.log({ dateString });
-
-    const res = await createCalendarEvent({ ...rest, date: newDateString });
+    const res = await createCalendarEvent({ ...rest, date: dateString });
     if (!res?.message) {
       // reruns on the server and refreshes just the necessary parts.
       router.refresh();
