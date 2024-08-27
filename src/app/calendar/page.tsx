3489a6a1797bf1db566c2ff00f6f1a97f775c0bb
Jonathan Nielsen <jonathan.sparvath@gmail.com>
2024-08-27 13:25:54 +0200

    fixed date grouping to include year

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,5 +1,5 @@
 import { CalendarIcon } from "@radix-ui/react-icons";
-import { format, getISOWeek } from "date-fns";
+import { format, getISOWeek, getISOWeekYear } from "date-fns";
 import Link from "next/link";
 import { CreateCalendarEventOverlay } from "~/app/calendar/_components/CreateCalendarEventOverlay";
 import { Card, CardContent } from "~/components/ui/card";
@@ -36,81 +36,116 @@ function CalendarEvent({
             <p className="line-clamp-2">{description}</p>
             <p className="justify-self-end">{time}</p>
           </div>
-          {/* <p className="ml-2 mt-2 text-sm"> - {personQuoted}</p> */}
         </div>
       </CardContent>
     </Card>
   );
 }
-function initialiseWeekGroup(firstWeekNumber: number, lastWeekNumber: number) {
-  const groupedByWeek = new Map<number, GetCalendarEventsReturnType[]>();
-  let week = firstWeekNumber;
-  while (week <= lastWeekNumber) {
-    groupedByWeek.set(week, []);
-    week++;
+
+function initialiseYearGroup({
+  firstWeekYear,
+  lastWeekYear,
+}: {
+  firstWeekYear: number;
+  lastWeekYear: number;
+}) {
+  const groupedByYear: Map<
+    number,
+    Map<number, GetCalendarEventsReturnType[]>
+  > = new Map<number, Map<number, GetCalendarEventsReturnType[]>>();
+  let year = firstWeekYear;
+  while (year <= lastWeekYear) {
+    groupedByYear.set(year, new Map());
+    year++;
   }
-  return groupedByWeek;
+  return groupedByYear;
 }
 
-function groupCalendarEventsByWeek(
-  calendarEvents: GetCalendarEventsReturnType[],
-) {
+function groupCalendarEvents(calendarEvents: GetCalendarEventsReturnType[]) {
   if (!calendarEvents.length) return [];
 
-  const firstEventWeekNumber = getISOWeek(new Date(calendarEvents[0]!.date));
-  const lastEventWeekNumber = getISOWeek(
+  const firstWeekNumber = getISOWeek(new Date(calendarEvents[0]!.date));
+  const firstWeekYear = getISOWeekYear(new Date(calendarEvents[0]!.date));
+
+  const lastWeekYear = getISOWeekYear(
     new Date(calendarEvents[calendarEvents.length - 1]!.date),
   );
 
-  const groupedByWeek = initialiseWeekGroup(
-    firstEventWeekNumber,
-    lastEventWeekNumber,
-  );
-
-  calendarEvents.forEach((calendarEvent) => {
-    const weekNumber = getISOWeek(new Date(calendarEvent.date));
-    groupedByWeek.get(weekNumber)?.push(calendarEvent);
+  const groupedByYear = initialiseYearGroup({
+    firstWeekYear,
+    lastWeekYear,
   });
-  return groupedByWeek;
+
+  let prevWeek = firstWeekNumber;
+
+  // for each event, check the week, if the week has a gap from the previous weeks, add whose weeks
+  calendarEvents.forEach((calendarEvent) => {
+    const date = new Date(calendarEvent.date);
+    const weekNumber = getISOWeek(date);
+    const weekYear = getISOWeekYear(date);
+
+    if (weekNumber > prevWeek + 1) {
+      let week = prevWeek + 1;
+      while (week < weekNumber) {
+        groupedByYear.get(weekYear)?.set(week, []);
+        week++;
+      }
+    }
+
+    if (!groupedByYear.get(weekYear)?.get(weekNumber)) {
+      groupedByYear.get(weekYear)?.set(weekNumber, []);
+    }
+    groupedByYear.get(weekYear)?.get(weekNumber)?.push(calendarEvent);
+
+    prevWeek = weekNumber;
+  });
+  return groupedByYear;
 }
 
 export default async function CalendarPage() {
   if (!isLoggedIn()) return null;
   const calendarEvents = await getCalendarEvents();
-  const groupedCalendarEvents = groupCalendarEventsByWeek(calendarEvents);
-
-  // sort calendar events into weeks
-  // Take first event, and last event. Calculate the weekend numbers til og med, and all the weeks in between.
+  const groupedByYear = groupCalendarEvents(calendarEvents);
 
   return (
     <main className="page-side-margin-1 page-bottom-nav-offset">
-      {Array.from(groupedCalendarEvents.entries()).map(
-        ([weekNumber, events]) => {
-          return (
-            <div key={weekNumber} className={`${events.length ? "mb-10" : ""}`}>
-              <div className="mb-4 flex items-center gap-2 font-medium text-muted-foreground">
-                <CalendarIcon className="h-5 w-5" />
-                Week {weekNumber}
-              </div>
-              <div className="flex flex-col gap-4">
-                {events.map((calendarEvent) => {
-                  return (
-                    <Link
-                      href={`/calendar/${calendarEvent.id}`}
-                      key={calendarEvent.id}
-                    >
-                      <CalendarEvent
-                        key={calendarEvent.id}
-                        {...calendarEvent}
-                      />
-                    </Link>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        },
-      )}
+      {Array.from(groupedByYear.entries()).map(([year, weeks], index) => {
+        return (
+          <div key={year}>
+            {index > 0 && (
+              <div className="mb-6 mt-3 text-3xl font-medium">{year}</div>
+            )}
+            {Array.from(weeks.entries()).map(([weekNumber, events]) => {
+              return (
+                <div
+                  key={weekNumber}
+                  className={`${events.length ? "mb-10" : ""}`}
+                >
+                  <div className="mb-4 flex items-center gap-2 font-medium text-muted-foreground">
+                    <CalendarIcon className="h-5 w-5" />
+                    Week {weekNumber}
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    {events.map((calendarEvent) => {
+                      return (
+                        <Link
+                          href={`/calendar/${calendarEvent.id}`}
+                          key={calendarEvent.id}
+                        >
+                          <CalendarEvent
+                            key={calendarEvent.id}
+                            {...calendarEvent}
+                          />
+                        </Link>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
       <div className="fixed bottom-28 right-8">
         <CreateCalendarEventOverlay />
       </div>
